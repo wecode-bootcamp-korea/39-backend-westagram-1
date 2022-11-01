81fc15ba55dc86ec3ebccf796a86b338b8e2ec80
MatheGoD <mathe1303@gmail.com';:Wq:wq>
2022-11-01 18:47:47 +0900

    [오현상-team1] Assignment 1 : Express API - 초기 환경 세팅(수정본)

--- HYUNSANG_OH/app.js
+++ HYUNSANG_OH/app.js
@@ -2,11 +2,11 @@ const http = require("http");
 const express = require("express");
 const cors = require ("cors");
 const morgan = require ("morgan");
-
 const dotenv = require ("dotenv");
+const { DataSource } = require('typeorm');
+
 dotenv.config()
 
-const { DataSource } = require('typeorm');
 
 const myDataSource = new DataSource({
     type: process.env.TYPEORM_CONNECTION,
@@ -23,9 +23,8 @@ myDataSource.initialize()
     })
 
 app = express()
-app.use(express.json()); // 외부에서 들어오는 요청 값 바디를 parsing하여 데이터 구조를 가독성 좋게 구조화시키는 것
-app.use(cors());  //프론트와 백의 서버가 서로 잘 소통 될 수 있도록 도와주는 언어
-// cors 메소드는 함수의 포함시켜 부분 적용 가능 
+app.use(express.json()); 
+app.use(cors());   
 app.use(morgan('dev'));
 
 dotenv.config();
