ac7a91d44f4686b87fc66581f8861d8fcc6333da
sangukpark <pso0301@gmail.com>
2022-11-03 20:51:31 +0900

    [박상욱-team1] Assignment1 : Express API - 초기 환경 세팅(수정사항 반영)

--- sanguk/db/migrations/20221103072556_create_books_authors_table.sql
+++ sanguk/db/migrations/20221103072556_create_books_authors_table.sql
@@ -1,11 +1,4 @@
 -- migrate:up
-CREATE TABLE books_authors (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    book_id INT NOT NULL,
-    authors_id INT NOT NULL,
-    CONSTRAINT books_authors_book_id_fkey FOREIGN KEY (book_id) REFERENCES books(id),
-    CONSTRAINT books_authors_author_id_fkey FOREIGN KEY (authors_id) REFERENCES authors(id)
-)
+
 
 -- migrate:down
-DROP TABLE books_authors;
