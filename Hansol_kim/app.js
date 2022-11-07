0ce10dc45fb5dfbea598d2a6500e4ba4cc964944
KHS <dev0226@gmail.com>
2022-11-07 17:34:18 +0900

    second assignment

--- Hansol_kim/app.js
+++ Hansol_kim/app.js
@@ -1,47 +1,69 @@
-const http = require("http");
 const express = require ("express");
+
 const cors = require("cors");
 const morgan = require("morgan");
 
-const dotenv = require("dotenv");
-dotenv.config()
+require("dotenv").config()
 
 const { DataSource } = require('typeorm')
 
-console.log( process.env.TYPEORM_PASSWORD)
-
 const myDataSource = new DataSource({
-
     type : process.env.TYPEORM_CONNECTION,
     host : process.env.TYPEORM_HOST,
-    port : process.env.TYPEORM_PORT,
+    PORT : process.env.TYPEORM_PORT,
     username : process.env.TYPEORM_USERNAME,
     password : process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE,
+    database: process.env.TYPEORM_DATABASE
 })
 
 myDataSource.initialize()
   .then(() => {
-    console.log("Data Source has been initialized!");
-  });
+    console.log("Data Source has been initialized!")
+  })
+  .catch((err) => {
+      console.error("Error during Data Source initialization", err)
+  myDataSource.destroy()
+  })
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT
 
-app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+app.use(express.json());
 
-
+// health check
 app.get("/ping", (req, res) => {
-    res.json({ message : "pong"});
-});
+    res.status(201).json({"message" : "pong"});
+})
 
 
-const server = http.createServer(app)
-const PORT = process.env.PORT;
+//create account
+app.post("/users/signup", async (req, res, next) => {
+  const {name, email, profileImage, password} = req.body
+  console.log(req);
+
+  //console.log(req)    
+
+  await myDataSource.query(
+    `INSERT INTO users(
+      name, 
+      email, 
+      profile_image, 
+      password
+  ) VALUES (?, ?, ?, ?);
+  `,
+  [name, email, profileImage, password])
+
+  res.status(201).json({ message : "userCreated"});
+})
 
 const start = async () => {
-    server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
-}
+  try {
+    app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 start()
