fae05800a9849b2a22e06f76d1d093b8b038506f
sangukpark <pso0301@gmail.com>
2022-11-09 15:44:59 +0900

    [박상욱-team1] Assignment3-5 : 과제통합- 수정완료- 재수정완료최종

--- sanguk/db/migrations/20221107153358_posts.sql
+++ sanguk/db/migrations/20221107153358_posts.sql
@@ -1,7 +1,6 @@
 -- migrate:up
     CREATE TABLE posts (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    postingId INT NOT NULL,
     Image_Url VARCHAR(500) NOT NULL,
     Content VARCHAR(1000) NOT NULL,
     userId INT NOT NULL,
